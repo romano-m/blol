976591e1258b3de7497a101106524a86b044534f
Romain Monclus <romain.monclus@gmail.com>
2014-06-17 11:23:43 +0200

    second commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+		return 'Index Page'
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
